6077ae1439fa704878fe4088bc5118f1a9fd5a7a
Rohit Badyal <rohitbadyal@Rohits-MacBook-Pro.local>
2021-07-15 16:41:12 -0400

    Updates

---
+++ precache-manifest.5fbdc8c90f31eee1a1fa189dc2b6d777.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f86041d13ed21c11c567406da6a403",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "d96477d34453172e9e15",
+    "url": "/airbnb-clone/static/css/10.102c59ca.chunk.css"
+  },
+  {
+    "revision": "500c1483ffcc8b6ac836",
+    "url": "/airbnb-clone/static/css/11.1dbb46a8.chunk.css"
+  },
+  {
+    "revision": "45934c7ae984ac339998",
+    "url": "/airbnb-clone/static/css/12.666b4bb1.chunk.css"
+  },
+  {
+    "revision": "2e969b2dff55396839b5",
+    "url": "/airbnb-clone/static/css/13.155a852d.chunk.css"
+  },
+  {
+    "revision": "a2e6562a829ed69b02d0",
+    "url": "/airbnb-clone/static/css/14.a782d88e.chunk.css"
+  },
+  {
+    "revision": "bbf6d4022905876180a0",
+    "url": "/airbnb-clone/static/css/3.1d89277d.chunk.css"
+  },
+  {
+    "revision": "160a76f755269e56a136",
+    "url": "/airbnb-clone/static/css/7.5c131d9a.chunk.css"
+  },
+  {
+    "revision": "d693a6aa9aa1f4c819f2",
+    "url": "/airbnb-clone/static/css/8.facbd943.chunk.css"
+  },
+  {
+    "revision": "6c1a7b3add10be1edd8c",
+    "url": "/airbnb-clone/static/css/9.aed570e8.chunk.css"
+  },
+  {
+    "revision": "10f22f8e223f42280758",
+    "url": "/airbnb-clone/static/css/main.22b1b5f0.chunk.css"
+  },
+  {
+    "revision": "7d3ce45de0dae456ca6f",
+    "url": "/airbnb-clone/static/js/0.ff71a347.chunk.js"
+  },
+  {
+    "revision": "712bd5597051b01c1c54",
+    "url": "/airbnb-clone/static/js/1.e01156d3.chunk.js"
+  },
+  {
+    "revision": "d96477d34453172e9e15",
+    "url": "/airbnb-clone/static/js/10.d1a29954.chunk.js"
+  },
+  {
+    "revision": "500c1483ffcc8b6ac836",
+    "url": "/airbnb-clone/static/js/11.045fa995.chunk.js"
+  },
+  {
+    "revision": "45934c7ae984ac339998",
+    "url": "/airbnb-clone/static/js/12.d3680e67.chunk.js"
+  },
+  {
+    "revision": "2e969b2dff55396839b5",
+    "url": "/airbnb-clone/static/js/13.a6aa9022.chunk.js"
+  },
+  {
+    "revision": "a2e6562a829ed69b02d0",
+    "url": "/airbnb-clone/static/js/14.42dc2856.chunk.js"
+  },
+  {
+    "revision": "48181234467ed67c675c",
+    "url": "/airbnb-clone/static/js/2.382fb932.chunk.js"
+  },
+  {
+    "revision": "bbf6d4022905876180a0",
+    "url": "/airbnb-clone/static/js/3.74f8db2d.chunk.js"
+  },
+  {
+    "revision": "e0fc2574b51973ccbf64",
+    "url": "/airbnb-clone/static/js/6.9426baa6.chunk.js"
+  },
+  {
+    "revision": "160a76f755269e56a136",
+    "url": "/airbnb-clone/static/js/7.651bb92b.chunk.js"
+  },
+  {
+    "revision": "d693a6aa9aa1f4c819f2",
+    "url": "/airbnb-clone/static/js/8.12e1bdd2.chunk.js"
+  },
+  {
+    "revision": "6c1a7b3add10be1edd8c",
+    "url": "/airbnb-clone/static/js/9.bea22962.chunk.js"
+  },
+  {
+    "revision": "10f22f8e223f42280758",
+    "url": "/airbnb-clone/static/js/main.c6c9b5e1.chunk.js"
+  },
+  {
+    "revision": "e77a3ca0ebdc72935a8d",
+    "url": "/airbnb-clone/static/js/runtime-main.cb0195fa.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/airbnb-clone/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/airbnb-clone/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/airbnb-clone/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/airbnb-clone/static/media/slick.f97e3bbf.svg"
+  }
+]);
